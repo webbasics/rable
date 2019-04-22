897fd1bc5eabfb930b32f6a42b9a44f948dc2064
Kearfy <mt.dev@hotmail.com>
2019-04-22 16:24:36 +0200

    update README.md and uploaded src/rable.js

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Usefull functions for js
 - [Installation](#Installation)
 - _Documentation_
   * [Javascript](#Javascript)
+  * [PHP](#PHP)
+  * [CSS](#CSS)
 
 ## Installation
 
@@ -44,9 +46,9 @@ __Notes__
    ```
 
 * _prototype functions_
-  
+
     __Note: prototype function work as shown here: ```object.yourPrototypeFunction()```__
-    
+
     - __.switch()__
       _applied on Booleans only_
       ```javascript
@@ -55,61 +57,61 @@ __Notes__
       ```
     - __.valid( String: factor = "NU" )__
       _applied on Booleans, Strings, Numbers, Functions, Arrays and Objects_
-      
+
       __Factor__
         - N/n: null
         - E/e: empty
         - U/u: undefined
         - F/f: false
-        
+
         __Once an other factor has been set, the default factor will be overwritten!__
-      
+
       ```javascript
         false.valid()                         //true, because the false factor has not been set.
         false.valid("F")                      //false, because the false factor has been set.
-        
+
         "".valid()                            //false, because the empty factor has not been set.
         "".valid("E")                         //true, because the empty factor has been set.
       ```
     - __.is { Boolean / String / Number / Function / Array / Object / HTMLElement / NodeList }()__
       _Applied on Booleans, Strings, Numbers, Functions, Arrays, Objects, HTMLElements and NodeLists_
-      
+
       __Note: Some of these examples are invalid and will first need to be defined in an variable!__
-      
+
       ```javascript
         //.isBoolean
           true.isBoolean()                    //true
-        
+
         //.isString
           "my string".isString()              //true
-          
+
         //.isNumber
           1.isNumber()                        //true
-          
+
         //.isFunction()
           function() {}.isFunction()          //true
-          
+
         //.isArray()
           ['Array'].isArray()                 //true
-        
+
         //.isObject()
           {test: "Object"}.isObject()         //true
-          
+
         //.isHTMLElement()
           <element>.isHTMLElement()           //true
-          
+
         //.isNodeList()
           NodeList(0).isNodeList()            //true
-          
+
         //.is()
           "my string".is()                    //"String"
           true.is()                           //"Boolean"
       ```
     - __.isset( String: key, Boolean: search_val = false )__
       _Applied on Arrays, Objects and NodeLists_
-      
+
       __Note: Some examples are invalid and will first need to be defined in an variable!__
-      
+
       ```javascript
         //Array, search_val can affect the algorithm.
         ['val'].isset('val');                 //true, search_val will be true because its searching an array and input is a var
@@ -117,12 +119,12 @@ __Notes__
         ['val'].isset(0, true);               //false, search_val will be true because it has been told so. value 0 doesnt exist
         [2, 0].isset(0, true);                //true, because value 0 exists in array
         [].isset('val');                      //false, and it always will, because it's empty
-        
+
         //Object, search_val cannot (yet) affect the algorithm
         {test: "test"}.isset('test');         //true
         {test: "test"}.isset('srch');         //false
         {}.isset('test');                     //false, and it always will, because it's empty
-        
+
         //NodeList, search_val cannot (yet) affect the algorithm.
         NodeList(1) [div].isset(div);         //false, you cannot search by element name
         NodeList(1) [div].isset(0);           //true
@@ -131,27 +133,54 @@ __Notes__
       ```
     - __.spacing( Number: amount = 1, *NodeList* Number: item ( = Boolean: false))__
       _Applied on HTMLElements and NodeLists_
-      
+
       __Note: this will add the amount of ```<br>``` elements specified in 'amount'__
       __Note: Some example are invalid and will first need to be defined in a variable!__
-      
+
       ```javascript
        //HTMLElement
        <div></div>.spacing(2);                //<div><br><br></div>, placed two <br> elements
-       
+
        //NodeList
        NodeList(2) [div, div].spacing(1)      //(for both divs: ) <div><br></div>
-       NodeList(2) [div, div].spacing(2, 0)   //(only first div:) <div><br></div>, because you said it to only apply it to the first div
+       NodeList(2) [div, div].spacing(2, 0)   //(only first div:) <div><br><br></div>, because you said it to only apply it to the first div
       ```
     - __.geth( *NodeList* Number: item ( = Boolean: false) )__
       _Applied on HTMLElements and NodeLists_
-     
+
       __Note: geth stands for get html, it will return the content inside the element__
-     
+
       ```javascript
+       //HTMLElement
        <div>some content</div>.geth()          //"some content"
-      
-       //the content of div 1 will be "this is content 1" and for 2 "this is content 2"
+
+       //NodeList, the content of div 1 will be "this is content 1" and for 2 "this is content 2"
        NodeList(2) [div, div].geth()           //["this is content 1", "this is content 2"]
        NodeList(2) [div, div].geth(0)          //"this is content 1"
+      ```
+    - __.seth( String: content )__
+      _Applied on HTMLElements and NodeLists_
+
+      __Note: seth stands fot set html, this will overwrite the existing content__
+
+      ```javascript
+        //HTMLElement
+        <div>some content</div>.seth('new')    //<div>new</div>
+
+        //NodeList, the content of div 1 will be "this is content 1" and for 2 "this is content 2"
+        NodeList(2) [div, div].seth('new')      //NodeList(2) [div, div], where the content has been updated to 'new'
+        NodeList(2) [div, div].seth('new', 0)   //Nodelist(2) [div, div] where the content of only the first div has been set to 'new'
+      ```
+    - __.seti( String: newid )__
+      _Applied on HTMLElements and NodeLists_
+
+      __Note: seti stands for set id, this will overwrite the existing id__
+
+      ```javascript
+        //HTMLElement
+        <div id="myId"></div>.seti('myNewId')   //<div id="myNewId"></div>
+
+        //NodeList
+        NodeList(2) [div#d, div#d].seti('n')    //NodeList(2) [div#n, div#n]
+        NodeList(2) [div#d, div#d].seti('n', 0) //NodeList(2) [div#n, div#d]
       ```
---
+++ src/rable.js
@@ -0,0 +1,672 @@
+Boolean.prototype.switch = function() {
+    if (this.valueOf() === false) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+Boolean.prototype.valid =
+String.prototype.valid =
+Number.prototype.valid =
+Function.prototype.valid = function(type = true) {
+    return _.valid(this.valueOf(), type);
+}
+
+Array.prototype.valid =
+Object.prototype.valid = function() {
+    return true;
+}
+
+var funcs = ['isBoolean', 'isString', 'isNumber', 'isFunction', 'isArray', 'isObject', 'isHTMLElement', 'isNodeList', 'is'];
+funcs.forEach(function(func) {
+    Boolean.prototype[func] =
+    String.prototype[func] =
+    Number.prototype[func] =
+    Function.prototype[func] =
+    Array.prototype[func] =
+    Object.prototype[func] =
+    HTMLElement.prototype[func] =
+    NodeList.prototype[func] = function() {
+        return _[func](this.valueOf());
+    }
+});
+
+Array.prototype.isset =
+Object.prototype.isset =
+NodeList.prototype.isset = function(val, search_val = false) {
+    return _.isset(val, this.valueOf(), search_val);
+}
+
+//no specific catagorie
+
+HTMLElement.prototype.spacing = function(amt = 1) {                             //spacing ~> spacing
+    _.loop(amt, () => {this.adde('br');});
+    return this;
+}
+
+NodeList.prototype.spacing = function(amt, item = false) {
+    if (item.isNumber() && this.isset(item)) {
+        this[item].spacing(amt);
+    } else {
+        this.forEach(function(elmnt) {
+            elmnt.spacing(amt);
+        });
+    }
+    return this;
+}
+
+//element functions for obtaining information
+
+HTMLElement.prototype.geth = function() {                                       //geth ~> get html
+    return this.innerHTML;
+}
+
+NodeList.prototype.geth = function(item = false) {
+    var list = [];
+    this.forEach(function(elmnt) {
+        list.push(elmnt.geth());
+    });
+
+    if (item.isNumber() && _.valid(list[item])) {
+        return list[item];
+    } else {
+        return list;
+    }
+}
+
+//element functions for updating information
+
+HTMLElement.prototype.seth = function(content = '') {                                //seth ~> set html
+    this.innerHTML = content;
+    return this;
+}
+
+NodeList.prototype.seth = function(content = '', item = false) {
+    if (item.isNumber() && this.isset(item)) {
+        this[item].seth(content);
+    } else {
+        this.forEach(function(elmnt) {
+            elmnt.seth(content);
+        });
+    }
+
+    return this;
+}
+
+HTMLElement.prototype.seti = function(newid) {
+    this.id = newid;
+    return this;
+}
+
+NodeList.prototype.seti = function(newid, item = false) {
+    if (item.isNumber() && this.isset(item)) {
+        this[item].seti(newid);
+    } else {
+        this.forEach(function(elmnt) {
+            elmnt.seti(newid);
+        });
+    }
+
+    return this;
+}
+
+//element functions for adding new information
+
+HTMLElement.prototype.addh = function(content) {                                //addh ~> add html
+    this.seth(this.geth() + content);
+    return this;
+}
+
+NodeList.prototype.addh = function(content, item = false) {
+    if (item.isNumber() && this.isset(item)) {
+        this[item].addh(content);
+    } else {
+        this.forEach(function(elmnt) {
+            elmnt.addh(content);
+        });
+    }
+
+    return this;
+}
+
+HTMLElement.prototype.addc = function(newclass) {                               //addc ~> add class
+    if (newclass.isArray()) {
+        newclass.forEach(function(nwclss) {
+            this.addc(nwclss);
+        });
+    } else {
+        this.classList.add(newclass);
+    }
+
+    return this;
+}
+
+NodeList.prototype.addc = function(newclass, item = false) {
+    if (item.isNumber() && this.isset(item)) {
+        this[item].addc(newclass);
+    } else {
+        this.forEach(function(elmnt) {
+            elmnt.addc(newclass);
+        });
+    }
+
+    return this;
+}
+
+HTMLElement.prototype.adde = function(element, amt = false) {                                //adde ~> add element
+    var element = document.createElement(element);
+    this.appendChild(element);
+    return element;
+}
+
+NodeList.prototype.adde = function(element, item = false) {
+    if (item.isNumber() && this.isset(item)) {
+        return this[item].adde(element);
+    } else {
+        this.forEach(function(elmnt) {
+            elmnt.adde(element);
+        });
+
+        return this;
+    }
+}
+
+//element functions for checking information
+
+HTMLElement.prototype.hasc = function(theclass) {                               //hasc ~> has class
+    return this.classList.contains(theclass);
+}
+
+NodeList.prototype.hasc = function(theclass, item = false) {
+    if (item.isNumber() && this.isset(item)) {
+        return this[item].extc(theclass);
+    } else {
+        var tmp = [];
+        this.forEach(function(thclss) {
+            tmp.push(thclss.extc(theclass));
+        });
+        return tmp;
+    }
+}
+
+HTMLElement.prototype.hasi = function(id = false) {                               //hasi ~> has id
+    if (id === false) {
+        return this.id.valid('neuf');
+    } else {
+        return this.id === id;
+    }
+}
+
+//element functions for removing information
+
+HTMLElement.prototype.remc = function(oldclass) {                               //remc ~> remove class
+    if (oldclass.isArray()) {
+        oldclass.forEach(function(olclss) {
+            this.remc(olclss);
+        });
+    } else {
+        this.classList.remove(oldclass);
+    }
+
+    return this;
+}
+
+NodeList.prototype.remc = function(oldclass, item = false) {
+    if (item.isNumber() && this.isset(item)) {
+        this[item].remc(oldclass);
+    } else {
+        this.forEach(function(elmnt) {
+            elmnt.remc(oldclass);
+        });
+    }
+
+    return this;
+}
+
+HTMLElement.prototype.clean = function() {                                      //clean the element
+    this.seth('');
+    return this;
+}
+
+NodeList.prototype.clean = function(item = false) {
+    if (item.isNumber() && this.isset(item)) {
+        this[item].clean();
+    } else {
+        this.forEach(function(elmnt) {
+            elmnt.clean();
+        });
+    }
+
+    return this;
+}
+
+HTMLElement.prototype.remi = function() {                                       //remi ~> remove id
+    this.id = '';
+    return this;
+}
+
+NodeList.prototype.remi = function(item = false) {
+    if (item.isNumber() && this.isset(item)) {
+        return this[item].remi();
+    } else {
+        this.forEach(function(elmnt) {
+            elmnt.remi();
+        });
+
+        return this;
+    }
+}
+
+//element functions related to page-capturing and building
+
+HTMLElement.prototype.capture = function() {
+    return {
+        content: this.outerHTML
+    };
+}
+
+NodeList.prototype.capture = function(item = false) {
+    if (item.isNumber() && this.isset(item)) {
+        return {
+            content: this[item].outerHTML
+        }
+    } else {
+        var captures = [];
+        this.forEach(function(elmnt) {
+            captures.push({
+                content: elmnt.outerHTML
+            });
+        });
+        return captures;
+    }
+}
+
+HTMLElement.prototype.insert = function(data) {
+    if (data.isset('content') === false || data.isObject() === false) {
+        return null;
+    } else {
+        this.seth(data.content);
+    }
+}
+
+NodeList.prototype.insert = function(data, item = false) {
+    if (data.isset('content') === false || data.isObject() === false) {
+        return null;
+    } else {
+        if (item.isNumber() && this.isset(item)) {
+            this[item].seth(data.content);
+        } else {
+            this.forEach(function(elmnt) {
+                elmnt.seth(data.content);
+            });
+        }
+    }
+}
+
+//element functions for selecting elements
+
+HTMLElement.prototype.parent = function() {                                     //parent ~> parent
+    return this.parentElement;
+}
+
+NodeList.prototype.parent = function(item = false) {
+    if (item.valid('neuf') === false || this.isset(item) === false) {
+        item = 0;
+    }
+
+    if (this.isset(item) === false) {
+        return null;
+    } else {
+        return this[item];
+    }
+}
+
+HTMLElement.prototype.child = function(chld = false) {                          //child ~> child
+    var child = this.childNodes;
+    if (chld.isNumber() && child.isset(chld)) {
+        return child[chld];
+    } else {
+        return child;
+    }
+}
+
+NodeList.prototype.child = function(item = false, chld = false) {
+    if (item.isNumber() === false) {
+        item = 0;
+    }
+
+    if (this.isset(item)) {
+        return this[item].child(chld);
+    } else {
+        return null;
+    }
+}
+
+function _(element, item = false) {                                             //get element by name, classname or id, all means that you select all elements
+    if (element === document || element === 'document' || element === 'doc') {
+        return document;
+    } else {
+        var elements = document.querySelectorAll(element);
+        if (item.isNumber() && elements.isset(item)) {
+            return elements[item];
+        } else {
+            if (elements.length === 1) {
+                return elements[0];
+            } else {
+                return elements;
+            }
+        }
+    }
+}
+
+_.body = function() {                                                           //get body fast
+    return _('body');
+}
+
+_.doc = _.document = function() {                                               //get document fast
+    return document;
+}
+
+_.valid = function(input, type = true) {
+    type = (type.isString() === true ? type : (type === true ? 'NU' : ''));
+    if (type.includes("U") === true || type.includes('u') === true) {
+        if (input === undefined) {
+            return false;
+        }
+    }
+
+    if (type.includes("N") === true || type.includes('n') === true) {
+        if (input === null) {
+            return false;
+        }
+    }
+
+    if (type.includes("F") === true || type.includes('f') === true) {
+        if (input === false) {
+            return false;
+        }
+    }
+
+    if (type.includes("E") === true || type.includes('e') === true) {
+        if (input === "") {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+_.isBoolean = function(obj) {
+    if (obj.is() === "Boolean") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+_.isString = function(obj) {
+    if (obj.is() === "String") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+_.isNumber = function(obj) {
+    if (obj.is() === "Number") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+_.isFunction = function(obj) {
+    if (obj.is() === "Function") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+_.isArray = function(obj) {
+    if (obj.is() === "Array") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+_.isObject = function(obj) {
+    if (obj.is() === "Object") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+_.isHTMLElement = function(obj) {
+    if (obj.is() === "HTMLElement") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+_.isNodeList = function(obj) {
+    if (obj.is() === "NodeList") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+_.is = function(obj) {
+    return obj.constructor.name;
+}
+
+_.ready = function(func, obj = false) {
+    if (obj === document || obj === 'document' || obj === 'doc') {
+
+    } else if (obj.isHTMLElement()) {
+
+    } else if (obj.isNodeList()) {
+
+    }
+
+
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+_.isset = function(key, obj, search_val = false) {
+    if (obj.isObject() === true) {
+        return key in obj;
+    } else if (obj.isArray() === true) {
+        if (key.isNumber() === false) {
+            search_val = true;
+        }
+
+        if (search_val === true) {
+            return obj.includes(key);
+        } else {
+            return key in obj;
+        }
+    } else if (obj.isNodeList() === true) {
+        if (obj.item(key) === null) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return null;
+    }
+}
+
+_.fillObject = function(masterArr, newArr, validate = true) {
+    for(var key in newArr) {
+        if (!validate) {
+            masterArr[key] = newArr[key];
+        } else if (validate === true) {
+            if (newArr[key].valid('u') === true) {
+                masterArr[key] = newArr[key];
+            }
+        } else {
+            if (newArr[key].valid(validate) === true) {
+                masterArr[key] = newArr[key];
+            }
+        }
+    }
+
+    return masterArr;
+}
+
+_.log = function(msg, log = true) {
+    if (log === true) {
+        msg = '[ Toolkit.js ] ~> ' + msg;
+    }
+
+    console.log(msg);
+}
+
+_.loop = function(amt = 1, action = false) {
+    if (action.isFunction() === false) {
+        return null;
+    } else {
+        for (var i = 0; i < amt; i++) {
+            action();
+        }
+    }
+}
+
+_.redirect = function(options = '', blank = false) {
+    if (!options.isObject()) {
+        options = {
+            url: options,
+            blank: blank
+        };
+    }
+
+    options = _.fillObject({
+        url: document.URL,
+        data: false,
+        method: "post",
+        blank: blank
+    }, options, 'nuf');
+    options.method = (options.method.toLowerCase() !== 'post' ? (options.method.toLowerCase !== 'get' ? 'post' : 'get') : 'post');
+
+    if (options.data.isObject()) {
+        var form = document.createElement('form');
+        form.action = options.url;
+        form.method = options.method;
+        if (options.blank === true) {
+            form.target = '_blank';
+        }
+
+        for (var key in options.data) {
+            if (options.data.hasOwnProperty(key)) {
+                var input = document.createElement('input');
+                input.type = 'text';
+                input.name = key;
+                input.value = options.data[key];
+                form.appendChild(input);
+            }
+        }
+        document.body.appendChild(form);
+        form.submit();
+    } else {
+        if (options.blank === true) {
+            window.open(options.url, '_blank');
+        } else {
+            window.location.replace(options.url);
+        }
+    }
+}
+
+_.request = function(options) {
+    var defaultConfig = {
+        url: document.URL,
+        data: false,
+        method: 'post',
+        async: true,
+        beforeSend: false,
+        success: (response, status) => {
+            _.log('Succesfully obtained a response!');
+            _.log(response, false);
+        },
+        error: (status) => {
+            if (status === false) {
+                _.log('ERROR: Couldn\'t reach target!');
+            } else {
+                _.log('ERROR: The server has responded with status ' + status);
+            }
+        }
+    };
+
+    options = _.fillObject(defaultConfig, options, "nu");
+    options.url = (!options.url ? defaultConfig.url : options.url);
+    options.method = (options.method.toLowerCase() !== 'post' ? (options.method.toLowerCase() !== 'get' ? 'post' : 'get') : 'post');
+    options.async = (options.async.isBoolean() ? options.async : true);
+
+    var request = new XMLHttpRequest();
+
+    request.onload = () => {
+        if (request.status >= 200 && request.status < 400) {
+            if (_.valid(options.success, 'neuf')) {
+                var contentType = request.getResponseHeader("Content-Type");
+                if (contentType === 'application/json') {
+                    var response = JSON.parse(request.responseText);
+                } else {
+                    var response = request.responseText;
+                }
+                options.success(response, request.status);
+            }
+        } else {
+            if (_.valid(options.error, 'neuf')) {
+                options.error(request.status);
+            }
+        }
+    }
+
+    request.onerror = () => {
+        if (_.valid(options.error, 'neuf')) {
+            options.error(false);
+        }
+    }
+
+    if (_.valid(options.beforeSend, 'neuf')) {
+        options.beforeSend();
+    }
+
+    if (options.data.isObject()) {
+        var total = "";
+        for (var key in options.data) {
+            if (options.data.hasOwnProperty(key)) {
+                if (total === "") {
+                    total = total + key + '=' + options.data[key];
+                } else {
+                    total = total + '&' + key + '=' + options.data[key];
+                }
+            }
+        }
+
+        if (options.method === 'get') {
+            request.open(options.method, options.url + '?' + total, options.async);
+            request.send();
+        } else if (options.method === 'post') {
+            request.open(options.method, options.url, options.async);
+            request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+            request.send(total);
+        }
+    } else {
+        request.open(options.method, options.url, options.async);
+        request.send();
+    }
+}
+
+_.random = function(min, max, maxDecimal = 0) {
+    return parseFloat((Math.random() * (max - min) + min).toFixed(maxDecimal));
+}
